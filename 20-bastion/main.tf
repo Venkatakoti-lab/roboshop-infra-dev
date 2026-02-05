81f7a8f164c9885cda89b0ffccb026d9abb5c64b
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2026-02-05 10:12:48 +0530

    infra

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "bastion" {
   ami           = data.aws_ami.ami_id.id
   instance_type = "t3.micro"
-  vpc_security_group_ids = local.bastion_sg_id
+  vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id= local.public_subnet_ids
 
   tags = merge(
